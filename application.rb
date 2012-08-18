017d5e9b099a0b89b625c60ba5055ff66cd9b070
Brandon Liu <brandon.k.liu@gmail.com>
2012-08-18 10:59:39 +0800

    Saving state with the sidebar

--- application.rb
+++ application.rb
@@ -2,7 +2,14 @@
 
 require 'sinatra'
 require 'slim'
+require 'sass'
 
 get '/' do
   slim :home
+end
+
+get '/build/:name.css' do |name|
+  require './views/scss/bourbon/lib/bourbon.rb'
+  content_type :css
+  scss :"scss/#{name}", :layout => false
 end
