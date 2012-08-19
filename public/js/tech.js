bbcda04c62692ae3d9035bf730dfd9ab85c0b720
Brandon Liu <brandon.k.liu@gmail.com>
2012-08-19 23:48:14 +0800

    Add image slideshow

--- public/js/tech.js
+++ public/js/tech.js
@@ -1,6 +1,20 @@
 (function($) {
   $(document).ready(function() {
 
+    var SLIDESHOW_INTERVAL = 10000;
+
+    // Images
+    var image_ids = ["landing_img1", "landing_img2"];
+    var image_idx = 0;
+    setInterval(function() {
+      var cur_id = image_ids[image_idx];
+      image_idx = (image_idx + 1) % image_ids.length;
+      var next_id = image_ids[image_idx];
+      // Append next slide
+      $("#landing").append($("#landing_preparation #" + next_id).clone());
+      $("#landing #" + cur_id).slideUp(2000, function() { $(this).remove(); });
+    }, SLIDESHOW_INTERVAL);
+
     /*
 
     // Fixed TOC after scrolling to it
