95310e074500bca94b769deef2cec0568fe91eac
Brandon Liu <brandon.k.liu@gmail.com>
2012-09-08 16:24:02 -0400

    Add two pictures and web-optimized the size

--- public/js/tech.js
+++ public/js/tech.js
@@ -3,18 +3,22 @@
 
     var SLIDESHOW_INTERVAL = 10000;
 
-    // Images
-    var image_ids = ["landing_img1", "landing_img2"];
-    var image_idx = 0;
     setInterval(function() {
-      var cur_id = image_ids[image_idx];
-      image_idx = (image_idx + 1) % image_ids.length;
-      var next_id = image_ids[image_idx];
-      // Append next slide
-      $("#landing").append($("#landing_preparation #" + next_id).clone());
-      $("#landing #" + cur_id).slideUp(2000, function() { $(this).remove(); });
+      nextSlide();
     }, SLIDESHOW_INTERVAL);
   });
+
+  // Images
+  var image_ids = ["landing_img1", "landing_img2", "landing_img3", "landing_img4"];
+  var image_idx = 0;
+  function nextSlide() {
+    var cur_id = image_ids[image_idx];
+    image_idx = (image_idx + 1) % image_ids.length;
+    var next_id = image_ids[image_idx];
+    // Append next slide
+    $("#landing").append($("#landing_preparation #" + next_id).clone());
+    $("#landing #" + cur_id).slideUp(2000, function() { $(this).remove(); });
+  }
 
   function scrollTo(element) {
     $("html, body").animate({
