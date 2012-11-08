984c9d84d3c2fe2934359822fb726786f0e0efd9
Brandon Liu <brandon.k.liu@gmail.com>
2012-11-08 02:01:41 -0500

    Add a donate redirect

--- application.rb
+++ application.rb
@@ -17,7 +17,8 @@ end
 redirects = {
   "/initial_proposal" => "https://docs.google.com/document/d/11-5EMSkbsVMd6X6QUlOgnrqSKC4-I284Ig9VzZhrtlU/edit",
   "/summary" =>  "https://docs.google.com/document/d/1GC1TlAeCeFyNOQzm5JDmQ7scqZY0-lv2q7SoquXc3KU/edit",
-  "/svc" => "https://docs.google.com/document/d/1gOnCZUtlVsw1aWU7hxu8u_sndhHc_RFm7r-TwwTk0Gs/edit"
+  "/svc" => "https://docs.google.com/document/d/1gOnCZUtlVsw1aWU7hxu8u_sndhHc_RFm7r-TwwTk0Gs/edit",
+  "/donate" => "http://www.causes.com/actions/1701185"
 }
 
 redirects.each do |short_link, full_link|
