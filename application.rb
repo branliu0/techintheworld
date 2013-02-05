81cbf250cfab4087468251706f253386dfb1ea6a
Brandon Liu <brandon.k.liu@gmail.com>
2013-02-04 19:02:24 -0500

    Add short link to fellowship signup google doc

--- application.rb
+++ application.rb
@@ -18,7 +18,8 @@ redirects = {
   "/initial_proposal" => "https://docs.google.com/document/d/11-5EMSkbsVMd6X6QUlOgnrqSKC4-I284Ig9VzZhrtlU/edit",
   "/summary" =>  "https://docs.google.com/document/d/1GC1TlAeCeFyNOQzm5JDmQ7scqZY0-lv2q7SoquXc3KU/edit",
   "/svc" => "https://docs.google.com/document/d/1gOnCZUtlVsw1aWU7hxu8u_sndhHc_RFm7r-TwwTk0Gs/edit",
-  "/donate" => "http://www.causes.com/actions/1701185"
+  "/donate" => "http://www.causes.com/actions/1701185",
+  "/fellowship" => "https://docs.google.com/a/techintheworld.org/spreadsheet/viewform?usp=sharing&formkey=dENtSk5iQ3pmemExYUt6eUY5NjY3dVE6MQ#gid=0"
 }
 
 redirects.each do |short_link, full_link|
