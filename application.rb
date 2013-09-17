225c36fa9b8ddbc768d3bf8077d118479e924bae
Victoria Gu <vgu@Victorias-MacBook-Pro.local>
2013-09-17 16:08:15 -0400

    fixed comma

--- application.rb
+++ application.rb
@@ -19,7 +19,7 @@ redirects = {
   "/summary" =>  "https://docs.google.com/document/d/1GC1TlAeCeFyNOQzm5JDmQ7scqZY0-lv2q7SoquXc3KU/edit",
   "/svc" => "https://docs.google.com/document/d/1gOnCZUtlVsw1aWU7hxu8u_sndhHc_RFm7r-TwwTk0Gs/edit",
   "/donate" => "http://www.causes.com/actions/1701185",
-  "/fellowship" => "https://docs.google.com/a/techintheworld.org/spreadsheet/viewform?usp=sharing&formkey=dENtSk5iQ3pmemExYUt6eUY5NjY3dVE6MQ#gid=0"
+  "/fellowship" => "https://docs.google.com/a/techintheworld.org/spreadsheet/viewform?usp=sharing&formkey=dENtSk5iQ3pmemExYUt6eUY5NjY3dVE6MQ#gid=0",
   "/application" => "https://docs.google.com/a/techintheworld.org/forms/d/1Tp24p5iWpaqsQOaxsBiz1Bpc3vXsZARZ3pygI5m7rPE/viewform"
 }
 
